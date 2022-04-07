a45dd475f9304c340e82c3fdf0f03012ee4a816f
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-04-07 16:53:58 -0500

    complete left bar selection feature.

--- src/components/LeftBar/LeftBar_List/LeftBar_List.js
+++ src/components/LeftBar/LeftBar_List/LeftBar_List.js
@@ -12,7 +12,9 @@ import * as Blueprint from '../../../utils/blueprint/blueprint';
 import cn from 'classnames';
 
 const recursiveItem = ({ item, depth, isOpen, initialOpenDepth }) => {
-  const [openChildren, setOpenChildren] = useState(isOpen);
+  const [openChildren, setOpenChildren] = useState(
+    isOpen && depth < initialOpenDepth,
+  );
 
   useEffect(() => {
     !isOpen && setOpenChildren(isOpen);
@@ -47,80 +49,78 @@ const LeftBar_List = () => {
 
   return (
     <div className="LeftBar_List">
-      {editMode === 'FLOOR PLAN' && (
-        <>
-          <div
-            className={cn('floor-plan-item')}
-            onClick={() => {
-              loadDefaultDesign();
+      <div className={cn('content', { active: editMode === 'FLOOR PLAN' })}>
+        <div
+          className={cn('floor-plan-item')}
+          onClick={() => {
+            loadDefaultDesign();
+          }}
+          title="New Design">
+          New Design
+        </div>
+        <div
+          className={cn('floor-plan-item')}
+          onClick={() => {
+            saveDesign();
+          }}
+          title="Save Design">
+          Save Design
+        </div>
+        <label className={cn('floor-plan-item')} htmlFor="upload">
+          Load Design
+          <input
+            id="upload"
+            style={{ display: 'none' }}
+            type="file"
+            onClick={(e) => {
+              e.target.value = null;
             }}
-            title="New Design">
-            New Design
-          </div>
-          <div
-            className={cn('floor-plan-item')}
-            onClick={() => {
-              saveDesign();
+            onChange={(e) => {
+              const [file] = e.target.files;
+              if (file) {
+                const reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = (e) => {
+                  loadDesign(e.target.result);
+                  return true;
+                };
+              }
             }}
-            title="Save Design">
-            Save Design
-          </div>
-          <label className={cn('floor-plan-item')} htmlFor="upload">
-            Load Design
-            <input
-              id="upload"
-              style={{ display: 'none' }}
-              type="file"
-              onClick={(e) => {
-                e.target.value = null;
-              }}
-              onChange={(e) => {
-                const [file] = e.target.files;
-                if (file) {
-                  const reader = new FileReader();
-                  reader.readAsText(file);
-                  reader.onload = (e) => {
-                    loadDesign(e.target.result);
-                    return true;
-                  };
-                }
-              }}
-            />
-          </label>
-          <div
-            className={cn('floor-plan-item', {
-              active: floorPlanMode === Blueprint.floorplannerModes.MOVE,
-            })}
-            onClick={() => {
-              updateFloorPlanMode(Blueprint.floorplannerModes.MOVE);
-            }}
-            title="Move Walls">
-            Move Walls
-          </div>
-          <div
-            className={cn('floor-plan-item', {
-              active: floorPlanMode === Blueprint.floorplannerModes.DRAW,
-            })}
-            onClick={() => {
-              updateFloorPlanMode(Blueprint.floorplannerModes.DRAW);
-            }}
-            title="Draw New Walls">
-            Draw New Walls
-          </div>
-          <div
-            className={cn('floor-plan-item', {
-              active: floorPlanMode === Blueprint.floorplannerModes.DELETE,
-            })}
-            onClick={() => {
-              updateFloorPlanMode(Blueprint.floorplannerModes.DELETE);
-            }}
-            title="Delete Walls">
-            Delete Walls
-          </div>
-        </>
-      )}
-      {editMode === '3D' &&
-        React.Children.toArray(
+          />
+        </label>
+        <div
+          className={cn('floor-plan-item', {
+            active: floorPlanMode === Blueprint.floorplannerModes.MOVE,
+          })}
+          onClick={() => {
+            updateFloorPlanMode(Blueprint.floorplannerModes.MOVE);
+          }}
+          title="Move Walls">
+          Move Walls
+        </div>
+        <div
+          className={cn('floor-plan-item', {
+            active: floorPlanMode === Blueprint.floorplannerModes.DRAW,
+          })}
+          onClick={() => {
+            updateFloorPlanMode(Blueprint.floorplannerModes.DRAW);
+          }}
+          title="Draw New Walls">
+          Draw New Walls
+        </div>
+        <div
+          className={cn('floor-plan-item', {
+            active: floorPlanMode === Blueprint.floorplannerModes.DELETE,
+          })}
+          onClick={() => {
+            updateFloorPlanMode(Blueprint.floorplannerModes.DELETE);
+          }}
+          title="Delete Walls">
+          Delete Walls
+        </div>
+      </div>
+      <div className={cn('content', { active: editMode === '3D' })}>
+        {React.Children.toArray(
           items.map((item) =>
             recursiveItem({
               item,
@@ -130,6 +130,7 @@ const LeftBar_List = () => {
             }),
           ),
         )}
+      </div>
     </div>
   );
 };
