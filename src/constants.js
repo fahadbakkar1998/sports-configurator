372e20edbda164b629be924951af798c466f4fe2
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-10-11 21:34:17 -0500

    update layer heights.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const decimalPlaces = 2;
 export const backendUrl = 'http://127.0.0.1:4000/products';
 export const groundLayerHeight = 0;
-export const floorLayerHeight = 4;
-export const itemLayerHeight = 8;
+export const floorLayerHeight = 6;
+export const itemLayerHeight = 12;
